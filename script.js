87fbac0d7cfbd29819442ae31dddc8519d19c592
erfan <erfanmhdipoor@gmail.com>
2023-01-17 12:13:39 +0330

    result to dom

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const caculate = () => {
     let number1 = parseInt(document.getElementById("number1").value)
     let number2 = parseInt(document.getElementById("number2").value)
     const opt = document.getElementById("operator").value
+    const result =document.getElementById("result")
+    console.log(result);
     console.log(opt);
     // switch(opt){
     //     case "+":
@@ -22,14 +24,21 @@ const caculate = () => {
     // }
     if (opt === "+") {
         console.log(number1 + number2);
+        result.innerText= number1+ number2
     } else if (opt === "-") {
         console.log(number1 - number2);
+        result.innerText= number1- number2
     } else if (opt === "*") {
         console.log(number1 * number2);
+        result.innerText= number1* number2
     } else if (opt === "/") {
         console.log(number1 / number2);
+        result.innerText= number1/number2
     } else {
         console.log("unknown opt");
+        result.innerText="unknown opt"
+        result.style.color="red"
+        alert("unknown opt")
     }
 
 
